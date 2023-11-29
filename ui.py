2071c07a5d1a1f793116a36638c0eae3e5638618
Wagnirskij <wagnirskij3@gmail.com>
2023-11-29 13:48:31 +0100

    add time tracking for total, fuckin around total and productive total

--- ui.py
+++ ui.py
@@ -10,6 +10,11 @@ class UserInterface:
         self.window.config(padx=20,pady=20,bg="white")
 
         self.start_time = None
+        self.good_time_start = None
+        self.bad_time_start = None
+
+        self.good_time_total = 0
+        self.bad_time_total = 0
 
         self.canvas = tk.Canvas(width=300,height=250,highlightthickness=0,bg="white")
         self.start_button = tk.Button(text="Start", command=self.timer_start)
@@ -21,10 +26,14 @@ class UserInterface:
         self.stop_button.grid(row=1,column=0)
         self.canvas.grid(row=1,column=0,  padx=20,pady=20)
 
-        
+        self.productive_button = tk.Button(text="productive", command=self.good_time_start_record)
+        self.productive_button.grid(row=3, column=0)
+
+        self.fuckin_around_button = tk.Button(text="fuckin around", command=self.bad_time_start_record)
+        self.fuckin_around_button.grid(row=3, column=1)
 
         self.time_label = tk.Label(text="Time: ", font=("Arial", 20), bg="blue", fg="white")
-        self.time_label.grid(row=0,column=1)
+        self.time_label.grid(row=0, column=1)
         self.window.mainloop()
 
     def timer_start(self) -> int:
@@ -42,6 +51,15 @@ class UserInterface:
             self.start_button.config(state=tk.NORMAL)
             self.stop_button.config(state=tk.DISABLED)
             self.window.after_cancel(self.update_id)
+            
+            if self.fuckin_around_button.cget("state") == "disabled":
+                self.bad_time_total += (datetime.now() - self.bad_time_start).seconds
+            elif self.productive_button.cget("state") == "disabled":
+                self.good_time_total += (datetime.now() - self.good_time_start).seconds
+
+            print(f"{self.elapsed_time.seconds=}")
+            print(f"{self.good_time_total=}")
+            print(f"{self.bad_time_total=}")
 
     def label_update(self):
         if self.start_time:
@@ -53,5 +71,25 @@ class UserInterface:
         pass
 
 
+    def good_time_start_record(self):
+        if self.bad_time_start:
+            self.bad_time_total += (datetime.now() - self.bad_time_start).seconds
+        self.good_time_start = datetime.now()
+        self.productive_button.config(state=tk.DISABLED)
+        self.fuckin_around_button.config(state=tk.NORMAL)
+        
+
+
+    def bad_time_start_record(self):
+        if self.good_time_start:
+            self.good_time_total += (datetime.now() - self.good_time_start).seconds
+        self.bad_time_start = datetime.now()
+        self.productive_button.config(state=tk.NORMAL)
+        self.fuckin_around_button.config(state=tk.DISABLED)
+
+
+
+
 ui = UserInterface()
 ui
+
